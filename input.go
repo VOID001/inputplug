72a8060c3c13446f52e7e306ddef3148843076ee
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2016-05-25 18:39:32 +0800

    Input Plugin Try#1

--- input.go
+++ input.go
@@ -1,8 +1,10 @@
 package sampinput
 
 import (
+	"errors"
 	"fmt"
 	"github.com/mozilla-services/heka/pipeline"
+	"os"
 )
 
 type SampleInputPlug struct {
@@ -10,14 +12,35 @@ type SampleInputPlug struct {
 	config   *SampleInputConfig
 }
 
-type SampleInputConfig struct {
-}
+type SampleInputConfig struct{}
 
 func init() {
-	fmt.Println("Init VOID001's Plugin Done")
 	pipeline.RegisterPlugin("SampleInput", func() interface{} { return new(SampleInputPlug) })
 }
 
 func (t *SampleInputPlug) Init(config interface{}) error {
+	var ok bool
+	conf := config.(pipeline.PluginConfig)
+	if t.filePath, ok = conf["filepath"].(string); !ok {
+		return errors.New("Now 'filepath' supported")
+	}
+	fmt.Println("Init Complete")
+	return nil
+}
+
+func (t *SampleInputPlug) Stop() {
+	return
+}
+
+func (t *SampleInputPlug) Run(ir pipeline.InputRunner, h pipeline.PluginHelper) (err error) {
+	//Access Data From Outside world
+	if _, err := os.Open(t.filePath); err != nil {
+		return fmt.Errorf("Open File %s error: %s", t.filePath, err.Error())
+	}
+	file, _ := os.Open(t.filePath)
+	for {
+		sr := ir.NewSplitterRunner("")
+		sr.SplitStream(file, nil)
+	}
 	return nil
 }
